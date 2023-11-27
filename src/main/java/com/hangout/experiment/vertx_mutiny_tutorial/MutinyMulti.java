ae0d12c90098fc74636346f0efcd17f3d8eb5abc
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2023-11-27 15:58:30 +0530

    Removed divide by 0 error

--- src/main/java/com/hangout/experiment/vertx_mutiny_tutorial/MutinyMulti.java
+++ src/main/java/com/hangout/experiment/vertx_mutiny_tutorial/MutinyMulti.java
@@ -14,7 +14,7 @@ public class MutinyMulti {
         // Multi represents a stream of elements. It can represent 0, 1, n or an
         // infinite number of items.
         Multi.createFrom().items(IntStream.rangeClosed(0, 10).boxed()).onItem()
-                .transform(value -> value % 2 == 0 ? value / 0 : value * 3 + 1).onItem()
+                .transform(value -> value % 2 == 0 ? value / 2 : value * 3 + 1).onItem()
                 .transform(String::valueOf)
                 // just subscribe to the last 2 entries
                 .select().last(4)
