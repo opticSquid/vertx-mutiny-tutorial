5ad8ae72696b57397db17c4fd8944b4069ee3a94
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2023-11-27 15:34:25 +0530

    Working with Uni streams in Mutiny

---
+++ src/main/java/com/hangout/experiment/vertx_mutiny_tutorial/MutinyUni.java
@@ -0,0 +1,17 @@
+package com.hangout.experiment.vertx_mutiny_tutorial;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.smallrye.mutiny.Uni;
+
+public class MutinyUni {
+    private final static Logger log = LoggerFactory.getLogger(MutinyUni.class);
+
+    public static void main(String[] args) {
+        Uni.createFrom().item("hello").onItem().transform(item -> item + " Mutiny!").onItem()
+                .transform(String::toUpperCase).subscribe().with(item -> log.debug("Item: {}", item));
+        Uni.createFrom().item("Ignored due to failure").onItem().castTo(Integer.class).subscribe()
+                .with(item -> log.info("Item: {}", item), failure -> log.error("Failure: {}", failure.getMessage()));
+    }
+}
